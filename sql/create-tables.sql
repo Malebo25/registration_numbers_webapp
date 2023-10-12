efddd39fa86d39f61aab2224425a261ab24291a2
malebo25 <malebomotlhake@gmail.com>
2023-10-12 15:57:39 +0200

    database queries

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -12,15 +12,6 @@ CREATE TABLE towns (
     town_name VARCHAR(255) NOT NULL
 );
 
--- Insert some sample town data
-INSERT INTO
-    towns (town_code, town_name)
-VALUES
-    ('ca', 'Town A'),
-    ('ck', 'Town B'),
-    ('cj', 'Town C'),
-    ('cl', 'Town D');
-
 CREATE TABLE registration_numbers (
     id SERIAL PRIMARY KEY,
     number_text VARCHAR(255) NOT NULL,
